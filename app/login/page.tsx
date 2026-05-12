005db8223f08754c2ff413264401fd4fa3ec6d44
Mosope11 <mosopefoluwaolatunji0@gmail.com>
2026-05-12 20:04:30 +0100

    Added Google adsense code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,23 @@ import { supabase } from '../supabase'
 import { useRouter } from 'next/navigation'
 import toast, { Toaster } from 'react-hot-toast'
 import { Eye, EyeOff } from 'lucide-react'
+import Script from 'next/script'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4905213854542365"
+          crossOrigin="anonymous"
+          strategy="afterInteractive" // Loads the script after the page is interactive
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
